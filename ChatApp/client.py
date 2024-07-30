d8e9d9926a0e2dfdcd0385138b3f45de7b281e28
arulvadivels <arulvadivel1103@gmail.com>
2024-07-30 13:57:50 +0530

    ChatApp Final changes without mthread

--- ChatApp/client.py
+++ ChatApp/client.py
@@ -1,17 +1,44 @@
 import socket
+from tkinter import *
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def send(listbox, entry):
+    # Send a message to the server
+    message = entry.get()
+    listbox.insert("end", "Me: "+message)
+    client_socket.send(bytes(message, "utf-8"))
+
+
+def receive(listbox):
+    # Receive a message from the client and set 100 for the length of the input
+    message_from_client = client_socket.recv(100)
+    listbox.insert("end", "Server: " + message_from_client.decode("utf-8"))
+
+
+# Set tkinter to show the messages
+root = Tk()
+root.title("Reciver")
+
+entry = Entry()
+entry.pack(side=BOTTOM)
+
+listbox = Listbox(root)
+listbox.pack()
+
+send_button = Button(root, text="Send", command=lambda: send(listbox, entry))
+send_button.pack(side=BOTTOM)
+
+receive_button = Button(root, text="Receive", command=lambda: receive(listbox))
+receive_button.pack(side=BOTTOM)
+
+# Socket connection to send and receive messages
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 IP = socket.gethostname()
 PORT = 8000
 
-server.connect((IP,PORT))
+# Bind IP address with Port
+client_socket.connect((IP, PORT))
 
-
-# import tkinter as tk
-
-# r = tk.Tk()
-# r.title('Counting Seconds')
-# button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-# button.pack()
-# r.mainloop()
+# Run a Tkinter
+root.mainloop()
--- ChatApp/server.py
+++ ChatApp/server.py
@@ -1,5 +1,36 @@
 import socket
+from tkinter import *
 
+
+def send(listbox, entry):
+    # Send a message to the client
+    message = entry.get()
+    listbox.insert("end", "Me: "+message)
+    client.send(bytes(message, "utf-8"))
+
+
+def receive(listbox):
+    # Receive a message from the client and set 100 for the length of the input
+    message_from_client = client.recv(100)
+    listbox.insert("end", "Client: " + message_from_client.decode("utf-8"))
+
+
+# Set tkinter to show the messages
+root = Tk()
+root.title("Server")
+entry = Entry()
+entry.pack(side=BOTTOM)
+
+listbox = Listbox(root)
+listbox.pack()
+
+send_button = Button(root, text="Send", command=lambda: send(listbox, entry))
+send_button.pack(side=BOTTOM)
+
+receive_button = Button(root, text="Receive", command=lambda: receive(listbox))
+receive_button.pack(side=BOTTOM)
+
+# Socket connection to send and receive messages
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 IP = socket.gethostname()
@@ -8,22 +39,10 @@ PORT = 8000
 # Bind IP address with Port
 server_socket.bind((IP, PORT))
 
-"""Provide maximum connectins to connect"""
+# Provide maximum connectins to connect
 server_socket.listen(4)
 
-while True:
-    client, client_address = server_socket.accept()
-    print(client,client_address)
-    False
-# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client, client_address = server_socket.accept()
 
-# # server_socket.bind(('localhost', 80))  # Use 0 to bind to any available port
-
-# # Get the IP address and port number
-# # ip_address, port = server_socket.getsockname()
-# # print(ip_address, port)
-
-# server_socket.bind((IP, PORT))
-
-# ip_address, port = server_socket.getsockname()
-# print(ip_address, port)
+# Run a Tkinter
+root.mainloop()
