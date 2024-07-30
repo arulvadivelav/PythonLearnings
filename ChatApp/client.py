57e118f95c3bdb8e4ae0040d417f0f3a8f8d359d
arulvadivels <arulvadivel1103@gmail.com>
2024-07-30 11:25:55 +0530

    gitReset

--- ChatApp/client.py
+++ ChatApp/client.py
@@ -5,13 +5,6 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = socket.gethostname()
 PORT = 8000
 
-server.connect((IP,PORT))
+#server.connect((IP,PORT))
 
 
-# import tkinter as tk
-
-# r = tk.Tk()
-# r.title('Counting Seconds')
-# button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-# button.pack()
-# r.mainloop()
